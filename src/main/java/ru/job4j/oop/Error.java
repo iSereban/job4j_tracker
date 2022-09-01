4aa4336b6fdc5bac7f8b8e90317b0b7edce790ee
iSereban <web-podderjka@mail.ru>
2022-09-01 11:22:04 +0300

    1.7. Конструктор

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,28 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+
+    }
+    public Error( boolean active, int status, String message){
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+    public void printInfo() {
+        System.out.println("Много мониторов: " + active);
+        System.out.println("SSD: " + status + " GB");
+        System.out.println("Модель CPU: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error error1 = new Error(true, 10, "Hello!");
+        error1.printInfo();
+    }
+}
