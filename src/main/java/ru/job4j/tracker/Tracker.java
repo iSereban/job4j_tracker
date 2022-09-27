3da1fdabac4a2fc470f90e23f634d0e329c57d63
iSereban <web-podderjka@mail.ru>
2022-09-27 18:56:12 +0300

    5. Tracker - хранилище

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -11,6 +11,29 @@ public class Tracker {
         return item;
     }
 
+    public Item[] findAll() {
+        Item[] tmp = new Item[items.length];
+        for (int i = 0, j = 0; i < tmp.length; i++) {
+            if (this.items[i] != null) {
+                tmp[j++] = items[i];
+            }
+        }
+        return tmp;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[0];
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null && items[i].getName().equals(key)) {
+                Item[] temp = new Item[result.length + 1];
+                System.arraycopy(result, 0, temp, 0, result.length);
+                temp[temp.length - 1] = items[i];
+                result = temp;
+            }
+        }
+        return result;
+    }
+
     public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
