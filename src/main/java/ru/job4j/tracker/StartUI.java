adf578a31b5f0de1e85f0c857ee6260a7183da97
iSereban <web-podderjka@mail.ru>
2022-11-07 20:31:43 +0300

    9. toString

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,10 +7,12 @@ public class StartUI {
 
     public static void main(String[] args) {
         Item items = new Item();
+        Item itemsSecond = new Item();
         LocalDateTime item = items.getCreated();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = item.format(formatter);
         System.out.println("Текущие дата и время: " + item);
         System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+        System.out.println(itemsSecond);
     }
 }
